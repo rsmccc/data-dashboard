d02f3fdab52b6439f6ea590ae2669db6d0fd6f5d
Riley McCloskey <rsmccc@iastate.edu>
2016-02-22 03:32:38 -0600

    improved

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 // Load the necessary servers.
 var sys = require( "sys" );
 var http = require( "http" );
+var fs = require( "fs" );
  
 // Create our HTTP server.
 var server = http.createServer(
-function( request, response ){
- 
- 
-// Create a SUPER SIMPLE response.
-response.writeHead( 200, {"content-type": "text/plain"} );
-response.write( "Hellow world from AWS!\n" );
-response.end();
- 
- 
-}
+	function( request, response ){
+	 
+	 
+	// Create a SUPER SIMPLE response.
+	response.writeHead( 200, {"content-type": "text/html"} );
+	// response.write( "Hellow world from AWS!\n" );
+	response.end( fs.readFileSync('index.html') );
+	 
+	 
+	}
 );
  
 // Point the HTTP server to port 8080.
 server.listen( 8080 );
  
 // For logging....
-sys.puts( "Server is running on 8080" );
+console.log( "Server is running on 8080" );
