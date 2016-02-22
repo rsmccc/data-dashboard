33dc738045c95656d1b50c2d722ca0c60922ca44
Riley McCloskey <rsmccc@iastate.edu>
2016-02-22 03:57:39 -0600

    finally

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 // Load the necessary servers.
-var sys = require( "sys" );
-var http = require( "http" );
-var fs = require( "fs" );
- 
+var sys = require("sys"),
+http = require("http"),
+fs = require( "fs" ),
+url = require("url"),
+path = require("path"),
+baseDirectory = __dirname; // /../.../data-dashboard
+
 // Create our HTTP server.
 var server = http.createServer(
-	function( request, response ){
-	 
-	 
-	// Create a SUPER SIMPLE response.
-	response.writeHead( 200, {"content-type": "text/html"} );
-	// response.write( "Hellow world from AWS!\n" );
-	response.end( fs.readFileSync('index.html') );
-	 
-	 
-	}
-);
- 
+	function( request, response ) { 
+		
+	});
+
 // Point the HTTP server to port 8080.
 server.listen( 8080 );
- 
+
 // For logging....
 console.log( "Server is running on 8080" );
