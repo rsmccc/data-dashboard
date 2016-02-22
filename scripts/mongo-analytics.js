810c347afa03895ba88aed14e8f8086750c89b9f
Riley McCloskey <rsmccc@iastate.edu>
2016-02-21 21:54:18 -0600

    stuff

---
+++ scripts/mongo-analytics.js
@@ -0,0 +1,36 @@
+// DataService.js
+
+
+// conn = new Mongo("host:port/collection");
+db = connect("rsmccloskey.me:27017/data", "tkeapp", "1Inspired!");
+// db.auth({})
+
+//initial config
+print("Collections in database:" + db.getCollectionNames());
+
+var c = db.members.find();
+
+print("has fbid: " + db.members.find({"fbid": {$exists: true}}).count());
+
+var names = db.members.distinct("name", {"fbid": {$exists: false}});
+
+var noFbId = db.members.find({"fbid": {$exists: false}}, {name: true, _id: false});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
