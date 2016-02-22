10b2e7f8777bd5945939efeea313318c43458d78
Riley McCloskey <rsmccc@iastate.edu>
2016-02-22 06:57:08 -0600

    hey

--- api/service/MongoService.js
+++ api/service/MongoService.js
@@ -1,21 +1,23 @@
 // DataService.js
 
+if (Mongo != undefined) {
 
-// conn = new Mongo("host:port/collection");
-db = connect("rsmccloskey.me:27017/data", "tkeapp", "1Inspired!");
-// db.auth({})
+	// conn = new Mongo("host:port/collection");
+	db = connect("rsmccloskey.me:27017/data", "tkeapp", "1Inspired!");
+	// db.auth({})
 
-//initial config
-print("Collections in database:" + db.getCollectionNames());
+	//initial config
+	print("Collections in database:" + db.getCollectionNames());
 
-var c = db.members.find();
+	var c = db.members.find();
 
-print("has fbid: " + db.members.find({"fbid": {$exists: true}}).count());
+	print("has fbid: " + db.members.find({"fbid": {$exists: true}}).count());
 
-var names = db.members.distinct("name", {"fbid": {$exists: false}});
+	var names = db.members.distinct("name", {"fbid": {$exists: false}});
 
-var noFbId = db.members.find({"fbid": {$exists: false}}, {name: true, _id: false});
+	var noFbId = db.members.find({"fbid": {$exists: false}}, {name: true, _id: false});
 
+}
 
 
 
