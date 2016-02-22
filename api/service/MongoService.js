c68b496688ada1340558a90bd2d26438c8df5adc
Riley McCloskey <rsmccc@iastate.edu>
2016-02-22 07:57:52 -0600

    mongo

--- api/service/MongoService.js
+++ api/service/MongoService.js
@@ -1,26 +1,42 @@
 // DataService.js
 
 if (Mongo != undefined) {
-
-	// conn = new Mongo("host:port/collection");
-	db = connect("rsmccloskey.me:27017/data", "tkeapp", "1Inspired!");
-	// db.auth({})
-
-	//initial config
-	print("Collections in database:" + db.getCollectionNames());
-
-	var c = db.members.find();
-
-	print("has fbid: " + db.members.find({"fbid": {$exists: true}}).count());
-
-	var names = db.members.distinct("name", {"fbid": {$exists: false}});
-
-	var noFbId = db.members.find({"fbid": {$exists: false}}, {name: true, _id: false});
-
+	mongoNative();
 }
 
 
+function mongoNative() {
+	db = connect("rsmccloskey.me:27017/data", "tkeapp", "1Inspired!");
+	// var c = db.members.find();
+	// while (c.hasNext()) {
+	// 	printjson(c.next());
+	// }
+	// var names = db.members.distinct("name", {"fbid": {$exists: false}});
+	// db.members.update()
+	var interact = db.runCommand({
+		group:
+		{
+			ns: 'posts',
+			key: { 'from.name': 1 },
+			$reduce: function ( curr, result ) {
+				result.posts++;
+				if (curr.likes !== undefined ) {
+					result.likes += curr.likes.data.length;
+				}
+				if (curr.comments !== undefined ) {
+					result.comments += curr.comments.data.length;
+				}
+			},
+			initial: { posts : 0, likes : 0, comments : 0}
+		}
+	});
 
+	printjson(interact);
+
+	var stats = interact.retval;
+
+	
+}
 
 
 
