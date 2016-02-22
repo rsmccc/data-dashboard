535c58e0e06842a0e25f3cabb8e91e96fccb1258
Riley McCloskey <rsmccc@iastate.edu>
2016-02-22 04:57:38 -0600

    favicon

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+// port to listen on
 process.env.PORT = '8080';
 
 // view engine setup
@@ -17,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
