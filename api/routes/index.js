28c473c39b712dcdf9f247f8ce292d8ee6ef97d3
Riley McCloskey <rsmccc@iastate.edu>
2016-02-22 17:56:24 -0600

    mongoservice things

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var mserv = require('../service/MongoService');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,5 +10,9 @@ router.get('/', function(req, res, next) {
 router.get('/chart', function(req, res, next) {
 	res.render('chart', {title: 'D3js Charts'});
 });
+
+router.get('/mongo', function(req, res, next) {
+	res.send(mserv.conns(mserv.inters));
+})
 
 module.exports = router;
--- api/service/MongoService.js
+++ api/service/MongoService.js
@@ -1,40 +1,26 @@
 // DataService.js
 
-if (Mongo != undefined) {
-	var nstats = nativeStats();
+var shell = false;
+
+if (Mongo != undefined && shell) {
+	var nstats = nativeGetInteractions();
 	// db.runCommand({ insert: nstats.retval });
 
 	printjson(nstats);
 
 	// db.int_stats.insert(nstats.retval);
+
 	// printjson(db.int_stats.find({}));
-}
 
-/*
-else {
-	var Mongo = require('mongodb').MongoClient;
-	var url = "mongodb://tkeapp:1Inspired!@rsmccloskey.me:27017/data";
-
-
-
-}
-
-var connect = function(query) {
-	Mongo.connect(url, function (err, db) {
-		query(db).then(function(data, err) {
-			db.close();
-			if (err) throw err;
-			return data;
-		});
-	});
-};
-
-var interactions = function(db) {
-	var col = db.collection('posts');
-	col.group({
-		{ 'from.name': 1 },
-		reduce: function ( curr, result ) {
-
+	function nativeCountLikesComments() {
+		var db = connect("rsmccloskey.me:27017/data", "tkeapp", "1Inspired!");
+		return db.runCommand({
+			group:
+			{
+				ns: 'posts',
+				key: {"from.name": 1},
+				$reduce: function ( curr, result ) {
+					result.posts++;
 					if (curr.likes !== undefined ) {
 						result.likes += curr.likes.data.length;
 					}
@@ -42,40 +28,97 @@ var interactions = function(db) {
 						result.comments += curr.comments.data.length;
 					}
 				},
-				initial: { posts : 0, likes : 0, comments : 0}
-				});
-	})
-};
-*/
-function nativeStats() {
-	db = connect("rsmccloskey.me:27017/data", "tkeapp", "1Inspired!");
-	return db.runCommand({
-		group:
-		{
-			ns: 'posts',
-			key: {"from.name": 1},
-			$reduce: function ( curr, result ) {
-				result.posts++;
-				if (curr.likes !== undefined ) {
-					result.likes += curr.likes.data.length;
+				initial: { posts : 0, likes : 0, comments : 0},
+				finalize: function(result) {
+					result.name = result["from.name"];
+					delete result["from.name"];
+				}
+			}
+		});
+	}
+
+	function nativeGetInteractions() {
+		var db = connect("rsmccloskey.me:27017/data", "tkeapp", "1Inspired!");
+		return db.runCommand({
+			group:
+			{
+				ns: 'posts',
+				key: {"from.name": true},
+				$reduce: function ( curr, result ) {
+					result.posts++;
+					if (curr.likes !== undefined ) {
+						result.likes = curr.likes.data;
+						for (var i = 0; i < curr.likes.data.length; i++) {
+							var lyk = curr.likes.data[i];
+							printjson(lyk);
+						// result.likes.push(lyk);
+					}
 				}
 				if (curr.comments !== undefined ) {
-					result.comments += curr.comments.data.length;
+					result.comments = curr.comments.data;
+					for (var j = 0; j < curr.comments.data.length; j++) {
+						var comms = curr.comments.data[j];
+						printjson(comms);
+						// result.comments.push(comms);
+					}
 				}
 			},
-			initial: { posts : 0, likes : 0, comments : 0},
+			initial: { posts : 0, likes : [], comments : []},
 			finalize: function(result) {
 				result.name = result["from.name"];
 				delete result["from.name"];
 			}
 		}
 	});
+	}
 }
 
 
+else {
+	var Mongo = require('mongodb').MongoClient;
+	var url = "mongodb://tkeapp:1Inspired!@rsmccloskey.me:27017/data";
+
+	connect(interactions, function( err, data) {
+		console.log(data);
+	});
+
+	var connect = function(query) {
+		Mongo.connect(url, function (err, db) {
+			query(db).then(function(data, err) {
+				db.close();
+				if (err) throw err;
+				return data;
+			});
+		});
+	};
 
 
+	var oneWayInteractions = function(db) {
+		Mongo.connect(url, function (err, db) {
+			var col = db.collection('posts');
+			return col.group(
+				{key: 'from.name' },
+				{reduce: function ( curr, result ) {
 
+					if (curr.likes !== undefined ) {
+						result.likes = curr.likes.data;
+					}
+					if (curr.comments !== undefined ) {
+						result.comments = curr.comments.data;
+					}
+				}},
+				{initial: { posts : 0, likes : 0, comments : 0}},
+				{finalize: function ( result ) {
+					result.name = result["from.name"];
+					delete result["from.name"];
+				}}
+				);	
+			db.close();
+		});
+	}	
+
+
+}
 
 
 
